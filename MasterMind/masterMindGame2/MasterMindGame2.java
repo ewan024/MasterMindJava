2a80ee4c310fdd277074c06a5924c6975dcf7402
ewan024 <1212081@student.roc-nijmegen.nl>
2024-09-13 15:56:54 +0200

    workspace klaargezet

---
+++ MasterMind/masterMindGame2/MasterMindGame2.java
@@ -0,0 +1,59 @@
+package masterMindGame2;
+
+public class MasterMindGame2
+{
+
+    public static void main(String[] args)
+    {
+	double breakerA1 = 1.1;
+	double breakerB1 = 1.2;
+	double breakerC1 = 1.3;
+	double breakerD1 = 1.4;
+	
+	double breakerA2 = 2.1;
+	double breakerB2 = 2.2;
+	double breakerC2 = 2.3;
+	double breakerD2 = 2.4;
+	
+	double breakerA3 = 3.1;
+	double breakerB3 = 3.2;
+	double breakerC3 = 3.3;
+	double breakerD3 = 3.4;
+	
+	double breakerA4 = 4.1;
+	double breakerB4 = 4.2;
+	double breakerC4 = 4.3;
+	double breakerD4 = 4.4;
+	
+	double breakerA5 = 5.1;
+	double breakerB5 = 5.2;
+	double breakerC5 = 5.3;
+	double breakerD5 = 5.4;
+	
+	double breakerA6 = 6.1;
+	double breakerB6 = 6.2;
+	double breakerC6 = 6.3;
+	double breakerD6 = 6.4;
+	
+	double breakerA7 = 7.1;
+	double breakerB7 = 7.2;
+	double breakerC7 = 7.3;
+	double breakerD7 = 7.4;
+	
+	double breakerA8 = 8.1;
+	double breakerB8 = 8.2;
+	double breakerC8 = 8.3;
+	double breakerD8 = 8.4;
+	
+	double breakerA9 = 9.1;
+	double breakerB9 = 9.2;
+	double breakerC9 = 9.3;
+	double breakerD9 = 9.4;
+	
+	double breakerA10 = 10.1;
+	double breakerB10 = 10.2;
+	double breakerC10 = 10.3;
+	double breakerD10 = 10.4;
+    }
+
+}
---
+++ MasterMind/src/masterMindGame2/MasterMindGame2.java
@@ -0,0 +1,61 @@
+package masterMindGame2;
+
+public class MasterMindGame2
+{
+
+    public static void main(String[] args)
+    {
+	String breakerA1 = "C";
+	String breakerB1 = "C";
+	String breakerC1 = "C";
+	String breakerD1 = "C";
+	
+	String breakerA2 = "C";
+	String breakerB2 = "C";
+	String breakerC2 = "C";
+	String breakerD2 = "C";
+	
+	String breakerA3 = "C";
+	String breakerB3 = "C";
+	String breakerC3 = "C";
+	String breakerD3 = "C";
+	
+	String breakerA4 = "C";
+	String breakerB4 = "C";
+	String breakerC4 = "C";
+	String breakerD4 = "C";
+	
+	String breakerA5 = "C";
+	String breakerB5 = "C";
+	String breakerC5 = "C";
+	String breakerD5 = "C";
+	
+	String breakerA6 = "C";
+	String breakerB6 = "C";
+	String breakerC6 = "C";
+	String breakerD6 = "C";
+	
+	String breakerA7 = "C";
+	String breakerB7 = "C";
+	String breakerC7 = "C";
+	String breakerD7 = "C";
+	
+	String breakerA8 = "C";
+	String breakerB8 = "C";
+	String breakerC8 = "C";
+	String breakerD8 = "C";
+	
+	String breakerA9 = "C";
+	String breakerB9 = "C";
+	String breakerC9 = "C";
+	String breakerD9 = "C";
+	
+	String breakerA10 = "C";
+	String breakerB10 = "C";
+	String breakerC10 = "C";
+	String breakerD10 = "C";
+	
+	System.out.println(breakerA1);
+    }
+
+}
